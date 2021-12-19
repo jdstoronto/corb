2ee212467049d1d16bda14588d73bf1c06291aa1
jdstoronto <jds@justindsmith.ca>
2021-12-19 16:05:11 -0500

    try to get shop to work

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -19,6 +19,9 @@ const headingStyles = {
 // markup
 const SupportPage = ({data}) => {
   console.log(data)
+  var contentArray = data.wpPage.content.split("\n")
+  contentArray = contentArray.filter(Boolean)
+  console.log(contentArray)
 
   useEffect(() => {console.log("test")});
 
@@ -26,10 +29,8 @@ const SupportPage = ({data}) => {
     <main style={pageStyles} >
       <Header />
       <title>Home Page</title>
-      <h3 style={headingStyles} dangerouslySetInnerHTML={{__html:data.wpPage.content}} />
-      <div id="products">
-        products
-      </div>
+      <h3 style={headingStyles} dangerouslySetInnerHTML={{__html:contentArray[0]}} />
+      <div data-block-name="woocommerce/all-products" data-columns="3" data-rows="3" data-align-buttons="false" data-content-visibility="{&quot;orderBy&quot;:true}" data-orderby="date" data-layout-config="[[&quot;woocommerce\/product-image&quot;],[&quot;woocommerce\/product-title&quot;],[&quot;woocommerce\/product-price&quot;],[&quot;woocommerce\/product-rating&quot;],[&quot;woocommerce\/product-button&quot;]]" className="wp-block-woocommerce-all-products wc-block-all-products" data-attributes="{&quot;alignButtons&quot;:false,&quot;columns&quot;:3,&quot;contentVisibility&quot;:{&quot;orderBy&quot;:true},&quot;isPreview&quot;:false,&quot;layoutConfig&quot;:[[&quot;woocommerce/product-image&quot;],[&quot;woocommerce/product-title&quot;],[&quot;woocommerce/product-price&quot;],[&quot;woocommerce/product-rating&quot;],[&quot;woocommerce/product-button&quot;]],&quot;orderby&quot;:&quot;date&quot;,&quot;rows&quot;:3}"></div>
     </main>
   )
 
