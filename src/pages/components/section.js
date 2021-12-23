812cc5dd5df00c14f807d644aa843048e080ba99
jdstoronto <jds@justindsmith.ca>
2021-12-23 18:30:11 -0500

    update leader to connect with wordpress

--- src/pages/components/section.js
+++ src/pages/components/section.js
@@ -5,22 +5,30 @@ import LeaderLine from "leader-line-new";
 
 const Section = (props) => {
     React.useEffect(() => {
-        const myLine = new LeaderLine(
-            document.getElementById(`${props.section.title}endpt1`),
-            document.getElementById(`${props.section.title}stpt1`),
-            {color: 'grey', size: 2}
-            
-          );
+        props.contents.map(content =>{
+            var myLine = new LeaderLine(
+                document.getElementById(`${content.Name}endpt1`),
+                document.getElementById(`${content.Name}stpt1`),
+                {color: 'grey', size: 2}
+              );
+            return myLine
+        })
+        
     })
 
     return(
-    <div key={props.section.title} id={props.section.title} className= "descriptionContainer">
+    <div key={`section_${props.section.title}`} id={props.section.title} className= "descriptionContainer">
         {props.section.title}
-        <div className='start pt' id={`${props.section.title}stpt1`} ></div>
-        <div className='leaderTitle'>
-            <div className='end pt' id={`${props.section.title}endpt1`}>1</div>
-            <div className='endTitle'>Camera</div>
-        </div>
+        {props.contents.map(content =>{
+            return <div>
+                        <div className='start pt' id={`${content.Name}stpt1`} style={{top:`${content.Top}%`, left:`${content.Left}%`}} ></div>
+                        <div className='leaderTitle'>
+                            <div className='end pt' id={`${content.Name}endpt1`}>{content.Number}</div>
+                            <div className='endTitle'>{content.Name}</div>
+                        </div>
+                    </div>
+        })}
+
         <div className="sectionDescription"> 
             <h5>Camera</h5>
             more description about camera hopefully this works as plan will also be adding a for loop
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,50 @@ const pageStyles = {
   fontFamily: "-apple-system, Roboto, sans-serif",
 }
 
+function objectFromArray(arr){
+  var result = [];
+  var i, j;
+  for (i = 1; i < arr.length; i++) {
+    let object = {} 
+    let items
+    items = arr[i]
+      for (j = 0; j < items.length; j++) {
+        let objName = arr[0][j]
+        let objValue = items[j]
+        //console.log(objName)
+        //console.log(objValue)
+        object[objName] = objValue
+      }
+    result.push(object)
+  }
+
+  return result;
+}
+
+function objectFromTable(table){
+  var results = []
+  var contents = table.slice(table.indexOf("<tbody>")+7,table.lastIndexOf("</tbody>")).split(`<tr>`)
+  contents.map(content=>{
+    let raw_items = content.split(`<td>`)
+    let items = []
+    raw_items.map(item=>{
+      if(item){
+        item = item.slice(0,item.indexOf(`<`))
+        items.push(item)
+      }
+    })
+    if(items.length){
+      results.push(items);
+    }
+  })
+
+  var objects = objectFromArray(results)
+  return objects
+  //console.log(objects)
+}
 // markup
 const IndexPage = ({data}) => {
-  //console.log(data)
+  console.log(data)
   const imageRes = data.allWpPage.edges.find(edge =>edge.node.title.includes(`Home`)).node.featuredImage.node.sourceUrl
   const allSections = data.allWpPost.edges.map(edge =>edge.node)
   const videoURL = data.allWpMediaItem.edges.find(edge => edge.node.title.includes("Video")).node.mediaItemUrl
@@ -51,7 +92,7 @@ const IndexPage = ({data}) => {
   return (
     <main style={pageStyles} >
       <Header sections ={allSections.map(section=>{return section.title})}/>
-      <h1 className="tagLine">Better Than You Came</h1>
+      <div className="tagLine" dangerouslySetInnerHTML={{__html:allSections[0].content}}></div>
       <div id= "featureVideoContainer">
         <video id="featureVideo" tabIndex="0" preload="true">
           <source  src={videoURL}></source>
@@ -61,7 +102,10 @@ const IndexPage = ({data}) => {
         <div className ="featureImage" id={allSections[0].title}>   
           <img src={imageRes} alt='feature CORB unit'/> 
         </div>
-        {postSections.map(section => (<Section section={section}/>))}
+        {postSections.map(section => {
+          let contents = objectFromTable(section.content)
+          return <Section section={section} contents={contents}/>
+        })}
       </div>
     </main>
   )
