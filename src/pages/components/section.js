fb919f0c42523d4c832e4f4b3d5181686dbd80ee
jdstoronto <jds@justindsmith.ca>
2021-12-24 08:27:17 -0500

    animate description box

--- src/pages/components/section.js
+++ src/pages/components/section.js
@@ -4,6 +4,9 @@ import { isMobile } from "react-device-detect";
 import LeaderLine from "leader-line-new";
 
 const Section = (props) => {
+    const [highlightNum, setHighlightNum] = React.useState(0);
+    const [selectNum, setSelectNum] = React.useState(0);
+
     React.useEffect(() => {
         props.contents.map(content =>{
             var myLine = new LeaderLine(
@@ -16,22 +19,30 @@ const Section = (props) => {
         
     })
 
+    const highlight =(num)=>{
+        console.log(this)
+        setHighlightNum(parseInt(num)-1)
+    }
+
+    const unhighlight =()=>{
+        setHighlightNum(null)
+    }
+
     return(
     <div key={`section_${props.section.title}`} id={props.section.title} className= "descriptionContainer">
-        {props.section.title}
         {props.contents.map(content =>{
             return <div>
                         <div className='start pt' id={`${content.Name}stpt1`} style={{top:`${content.Top}%`, left:`${content.Left}%`}} ></div>
                         <div className='leaderTitle'>
-                            <div className='end pt' id={`${content.Name}endpt1`}>{content.Number}</div>
+                            <div className='end pt' id={`${content.Name}endpt1`} value={content.Number} onMouseEnter={()=>highlight(content.Number)} onMouseLeave={()=>unhighlight()}>{content.Number}</div>
                             <div className='endTitle'>{content.Name}</div>
                         </div>
                     </div>
         })}
 
         <div className="sectionDescription"> 
-            <h5>Camera</h5>
-            more description about camera hopefully this works as plan will also be adding a for loop
+            <h5>{props.contents[(highlightNum?highlightNum:selectNum)].Name}</h5>
+            {props.contents[(highlightNum?highlightNum:selectNum)].Description}
         </div>
     </div>
     )
