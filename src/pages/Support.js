eab9426545e504f9edeb3f5df6a0ac71529e988c
jdstoronto <jds@justindsmith.ca>
2021-12-25 12:41:16 -0500

    update support page

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -1,19 +1,18 @@
 import * as React from "react"
 import { useEffect } from 'react';
 import Header from "./components/header"
+import Footer from "./components/footer"
 import { graphql, Link } from 'gatsby'
 import Img from "gatsby-image"
 
 // styles
 const pageStyles = {
-  color: "#232129",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  color: "#484849",
+  fontFamily: "sans-serif, serif",
 }
 const headingStyles = {
-  marginTop: 50,
-  marginBottom: 64,
-  maxWidth: 600,
-  position: "fixed",
+  marginTop: 80,
+  marginBottom: 50,
 }
 
 // markup
@@ -29,8 +28,12 @@ const SupportPage = ({data}) => {
     <main style={pageStyles} >
       <Header />
       <title>Home Page</title>
-      <h3 style={headingStyles} dangerouslySetInnerHTML={{__html:contentArray[0]}} />
+      <div className="supportIntro mx-auto">
+        <h4 style={headingStyles} dangerouslySetInnerHTML={{__html:contentArray[0]}} />
+      </div>
+      <div className="mx-auto" style={{textAlign:"center"}}><h1>SHOP</h1></div>
       <div data-block-name="woocommerce/all-products" data-columns="3" data-rows="3" data-align-buttons="false" data-content-visibility="{&quot;orderBy&quot;:true}" data-orderby="date" data-layout-config="[[&quot;woocommerce\/product-image&quot;],[&quot;woocommerce\/product-title&quot;],[&quot;woocommerce\/product-price&quot;],[&quot;woocommerce\/product-rating&quot;],[&quot;woocommerce\/product-button&quot;]]" className="wp-block-woocommerce-all-products wc-block-all-products" data-attributes="{&quot;alignButtons&quot;:false,&quot;columns&quot;:3,&quot;contentVisibility&quot;:{&quot;orderBy&quot;:true},&quot;isPreview&quot;:false,&quot;layoutConfig&quot;:[[&quot;woocommerce/product-image&quot;],[&quot;woocommerce/product-title&quot;],[&quot;woocommerce/product-price&quot;],[&quot;woocommerce/product-rating&quot;],[&quot;woocommerce/product-button&quot;]],&quot;orderby&quot;:&quot;date&quot;,&quot;rows&quot;:3}"></div>
+      <Footer />
     </main>
   )
 
