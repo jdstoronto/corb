004902c3d3ea40d76799189ff8722b9c2a7cfb0d
jdstoronto <jds@justindsmith.ca>
2022-02-07 20:55:17 -0500

    create react contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -48,7 +48,46 @@ const ContactPage = ({data}) => {
             <div dangerouslySetInnerHTML={{__html:contentArray[1]}}/>
           </div>
           <div className="col-lg-7">
-            <div id= "contactForm" dangerouslySetInnerHTML={{__html:form}} />
+            {/* <div id= "contactForm" dangerouslySetInnerHTML={{__html:form}} /> */}
+            <div className="wpforms-container wpforms-container-full">
+              <form className="wpforms-validate wpforms-form" method="post" action="#">
+                <div className="wpforms-field-container">
+                  <label className="wpforms-field-label">
+                    Name
+                    <span className="wpforms-required-label">*</span>
+                  </label>
+                  <div className="wpforms-field-row wpforms-field-medium">
+                    <div className="wpforms-field-row-block wpforms-first wpforms-one-half">
+                      <input type="text" className="wpforms-field-name-first wpforms-field-required" name="firstName"/>
+                      <label>First</label>
+                    </div>
+                    <div className="wpforms-field-row-block wpforms-one-half">
+                      <input type="text" className="wpforms-field-name-last wpforms-field-required"  name="lastName"/>
+                      <label>Last</label>
+                    </div>
+                  </div>
+                  <div className="wpforms-field wpforms-field-email">
+                    <label className="wpforms-field-label">
+                      Email
+                      <span className="wpforms-required-label">*</span>
+                    </label>
+                    <input type="email" className="wpforms-field-medium wpforms-field-required">
+                    </input>
+                  </div>
+                  <div className="wpforms-field wpforms-field-textarea">
+                    <label className="wpforms-field-label">
+                      Comment or Message
+                      <span className="wpforms-required-label">*</span>
+                    </label>
+                    <textarea className="wpforms-field-medium wpforms-field-required" name="message">
+                    </textarea>             
+                  </div>
+                </div>
+                <div className="wpforms-submit-container">
+                  <button className="wpforms-submit" type="submit">Submit</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
